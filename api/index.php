ad693447818a7cfff0bc40a2a99264472a42a303
robiningelbrecht <robin@baldwin.be>
2022-09-21 16:58:01 +0200

    Removed omethink

--- api/index.php
+++ api/index.php
@@ -4,6 +4,8 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Notion\Notion;
 use Notion\Databases\Query;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -13,9 +15,9 @@ $app = AppFactory::create();
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->safeLoad();
 
-$twig = new \Twig\Environment(new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/templates'));
+$twig = new Environment(new FilesystemLoader(dirname(__DIR__) . '/templates'));
 
-$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) use($twig) {
+$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) use ($twig) {
     $notion = Notion::create($_ENV['NOTION_API_SECRET']);
 
     $database = $notion->databases()->find($_ENV['NOTION_DATABASE_SALARY']);
@@ -25,18 +27,20 @@ $app->get('/', function (ServerRequestInterface $request, ResponseInterface $res
     );
 
     /** @var \Notion\Pages\Page $page */
+    $labels = $data = [];
     foreach ($result->pages() as $page) {
         /** @var \Notion\Pages\Properties\PropertyInterface[] $properties */
         $properties = $page->properties();
-        foreach ($properties as $property) {
-            $value = match ($property::class) {
-                'Notion\Pages\Properties\Title' => $property->toString(),
-                default => $property->toArray()[$property->property()->type()],
-            };
-        }
+        $labels[] = $properties['Month']->start()->format('Y m');
+        $data[] = $properties['Gross salary']->number();
     }
 
-    $response->getBody()->write($twig->render('salary-chart.html.twig'));
+    //var_dump($labels);
+
+    $response->getBody()->write($twig->render('salary-chart.html.twig', [
+        'labels' => '[' . implode(',', array_map(fn(string $label) => sprintf('"%s"', $label), $labels)) . ']',
+        'data' => '[' . implode(',', $data) . ']',
+    ]));
     return $response;
 });
 
