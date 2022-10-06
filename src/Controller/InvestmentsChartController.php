683803c9f569565f96028a2cae5a207dfe6c3ddf
robiningelbrecht <robin@baldwin.be>
2022-10-06 08:55:27 +0200

    Investment allocation

--- src/Controller/InvestmentsChartController.php
+++ src/Controller/InvestmentsChartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\ValueObject\Notion\NotionInvestmentsDatabaseId;
+use Notion\Databases\Query;
 use Notion\Notion;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -21,10 +22,39 @@ class InvestmentsChartController
 
     public function handleAllocationChart(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
-        $labels = ['IWDA', 'EMIM', 'VWCE', 'ETH'];
+        $database = $this->notion->databases()->find($this->notionInvestmentsDatabaseId);
+        $result = $this->notion->databases()->query(
+            $database,
+            Query::create(),
+        );
+
+        /** @var \Notion\Pages\Page $page */
+        $labels = $investedPerTicker = [];
+        $totalInvested = 0;
+        foreach ($result->pages() as $page) {
+            /** @var \Notion\Pages\Properties\PropertyInterface[] $properties */
+            $properties = $page->properties();
+            $tickerId = $properties['Ticker']->id();
+            $amountInvested = $properties['EUR invested']->number();
+
+            $labels[$tickerId] = $properties['Ticker']->name();
+            if (empty($investedPerTicker[$tickerId])) {
+                $investedPerTicker[$tickerId] = 0;
+            }
+            $investedPerTicker[$tickerId] += $amountInvested;
+            $totalInvested += $amountInvested;
+        }
+
+        // Calculate percentages and update labels.
+        foreach ($investedPerTicker as $tickerId => $amountInvested) {
+            $percentage = round(($amountInvested / $totalInvested) * 100, 2);
+            $investedPerTicker[$tickerId] = $percentage;
+            $labels[$tickerId] .= ' (' . $percentage . '%)';
+        }
+
         $datasets = [
             [
-                'data' => '[' . implode(',', [91, 6, 2, 1]) . ']',
+                'data' => '[' . implode(',', $investedPerTicker) . ']',
             ],
         ];
         $response->getBody()->write($this->twig->render('investment-allocation-chart.html.twig', [
